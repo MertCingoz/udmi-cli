804b3eed9b0b156168e9e2a9ee0a01279c758042
Mert Cingöz <mertcingoz@gmail.com>
2023-03-22 14:41:38 +0100

    Fix kill pid script

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 set -o errexit -o nounset
 
-for process in pubber validator sequencer; do
-  ps ax | pgrep "$process" | grep java | awk '{print $1}' | while read -r pid; do
+for process in pubber validator; do
+  ps ax | pgrep -af "$process" | grep java | awk '{print $1}' | while read -r pid; do
     kill "$pid"
   done
 done
