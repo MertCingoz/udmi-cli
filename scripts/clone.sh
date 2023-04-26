e9b3a4ca3f9b6c9db2bf117a12c5ea963ba551c0
Mert Cingöz <mertcingoz@gmail.com>
2023-04-26 16:36:35 +0200

    Fix periodic_scan sequence to expect correct messages

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -3,7 +3,21 @@ set -o errexit -o nounset
 
 git clone --branch "$UDMI_TAG" https://github.com/faucetsdn/udmi.git . || true
 git checkout --detach "$UDMI_VERSION"
-sed 's/= getDefaultInterface()/= "eth0"/g' pubber/src/main/java/daq/pubber/LocalnetManager.java > tmp
-mv tmp pubber/src/main/java/daq/pubber/LocalnetManager.java
+
+filePath=pubber/src/main/java/daq/pubber/LocalnetManager.java
+sed 's/= getDefaultInterface()/= "eth0"/g' "$filePath" > tmp
+mv tmp "$filePath"
+
+filePath=validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/DiscoverySequences.java
+sed '/int expected = SCAN_ITERATIONS/,/received <= expected + 1);/c\
+    Set<String> eventFamilies = receivedEvents.stream()\
+        .flatMap(event -> event.families.keySet().stream())\
+        .collect(Collectors.toSet());\
+    assertTrue(\"all requested families present\", eventFamilies.containsAll(families));\
+    Map<String, List<DiscoveryEvent>> receivedEventsGrouped = receivedEvents.stream()\
+        .collect(Collectors.groupingBy(e -> e.scan_family + "." + e.scan_addr));\
+    assertTrue("scan iteration", receivedEventsGrouped.values().stream().allMatch(list -> list.size() == SCAN_ITERATIONS));' "$filePath" > tmp
+mv tmp "$filePath"
+
 bin/clone_model
 bin/genkeys sites/udmi_site_model
