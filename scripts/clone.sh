c99735cc5d9178f287a1d3d18d8d87c62c79913c
Mert Cingöz <mertcingoz@gmail.com>
2023-05-01 10:33:02 +0200

    Sync for main repo

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -19,5 +19,27 @@ sed '/int expected = SCAN_ITERATIONS/,/received <= expected + 1);/c\
     assertTrue("scan iteration", receivedEventsGrouped.values().stream().allMatch(list -> list.size() == SCAN_ITERATIONS));' "$filePath" > tmp
 mv tmp "$filePath"
 
+filePath=validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/LocalnetSequences.java
+sed '/private void familyAddr(String family) {/,/() -> expected.equals(actual));/c\
+  private void familyAddr(String family) {\
+    java.util.Set expectedFamilies = deviceMetadata.localnet.families.keySet().stream()\
+            .filter(f -> f.contains(family)).collect(java.util.stream.Collectors.toSet());\
+    checkThat("expectedFamilies greater than 0", () -> expectedFamilies.size() > 0);\
+    expectedFamilies.forEach(expectedFamily -> {\
+      String expected = catchToNull(() -> deviceMetadata.localnet.families.get(expectedFamily).addr);\
+      if (expected == null) {\
+        throw new SkipTest(String.format("No %S address defined in metadata", expectedFamily));\
+      }\
+      untilTrue("localnet families available", () -> deviceState.localnet.families.size() > 0);\
+      String actual = catchToNull(() -> deviceState.localnet.families.get(expectedFamily).addr);\
+      checkThat(String.format("device %s address was %s, expected %s", expectedFamily, actual, expected),\
+              () -> expected.equals(actual));\
+    });' "$filePath" > tmp
+mv tmp "$filePath"
+
+filePath=validator/src/main/java/com/google/daq/mqtt/validator/ReportingDevice.java
+sed 's/previous.before(getThreshold(now))/true/g' "$filePath" > tmp
+mv tmp "$filePath"
+
 bin/clone_model
 bin/genkeys sites/udmi_site_model
