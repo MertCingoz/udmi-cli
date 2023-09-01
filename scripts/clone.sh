68521997c18c1f3a25dbfb28c4840ca2f889e248
Mert Cingöz <mertcingoz@gmail.com>
2023-09-01 19:54:12 +0200

    Use 1.4.2

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 set -o errexit -o nounset
 
-git clone --branch "$UDMI_TAG" https://github.com/faucetsdn/udmi.git . || true
+git clone "$UDMI_REPO" . || true
+git remote add upstream https://github.com/faucetsdn/udmi.git || true
+git fetch upstream
 git checkout --detach "$UDMI_VERSION"
 
 filePath=validator/src/main/java/com/google/daq/mqtt/validator/ReportingDevice.java
@@ -11,3 +13,7 @@ bin/clone_model
 mkdir -p /scripts/udmi_site_model/devices
 cp -R /scripts/udmi_site_model sites
 bin/genkeys sites/udmi_site_model
+
+python3 -m venv venv
+. ./venv/bin/activate
+pip3 install Jinja2
